6d0a6a90efae31a5fe3bef6b0b3481718487f6fa
Nikintelligence <80717681+Nikintelligence@users.noreply.github.com>
2021-11-03 16:25:56 +0300

    Counter using class

---
+++ project2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CounterClass from './components/counterClass';
+
+function App() {
+
+  return (
+    <div className="App">
+      <CounterClass/>
+    </div>
+  );
+}
+
+export default App;
